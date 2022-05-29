068e42a4c7c83396eb3a8db5a335646a0f154f94
Srinivasaraghavansj <47287887+Srinivasaraghavansj@users.noreply.github.com>
2022-05-29 23:07:06 +0530

    Add files via upload

--- Python Quiz based learning with maker/main_data_extractor_selenium.py
+++ Python Quiz based learning with maker/main_data_extractor_selenium.py
@@ -1,34 +1,30 @@
-# from bs4 import BeautifulSoup
-# import json
-# from pprint import pprint
-# import re
-
-# with open("2.html", "r", encoding='utf-8') as f:
-#     html_doc= f.read()
-
-# soup = BeautifulSoup(html_doc, 'html.parser')
-# # print(soup.prettify())
-# # data = json.loads(soup.find('script').text())
-# # print(data)
-# txt = "".join([str(i) for i in list(soup.findAll('script'))])
-
-# json_string = re.search(r'correct_answer', txt, flags=re.DOTALL)
-
-# print(json_string)
-# # json_data = json.loads('{' + json_string[1] + '}')
-
-# # pprint(json_data)s
-
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
 from time import sleep
 import os
 
+files_ = []
+
+directory = os.getcwd()
+for root, dirnames, filenames in os.walk(directory):
+    for filename in filenames:
+        if filename.endswith('.html'):
+            a = filename.replace(".html","")
+            try:
+                int(a)
+                files_.append(int(a))
+            except:
+                pass
+files_ = sorted(files_)
+
+lesson_number_above=0
+files_list = list(i for i in files_ if i > lesson_number_above)
+
 with open("data.csv", "w+", encoding='utf-8') as f:
     # f.write("correct_answer,question,right_answer_message,wrong_answer_message,button_1_text,button_2_text\n")
     f.write("{\n")
-    files_list = [2,3,4,9,10,12,13,14,15,16,17,20,24,25,28,29,30]
+    # files_list = [2,3,4,9,10,12,13,14,15,16,17,20,24,25,28,29,30]
 
     for i in files_list:
         driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -59,12 +55,12 @@ with open("data.csv", "w+", encoding='utf-8') as f:
         driver.quit()
 
         main_string = f"""{i}:{{
-correct_answer : {correct_answer},
-question:`{question}`,
-right_answer_message:`{right_answer_message}`,
-wrong_answer_message:`{wrong_answer_message}`,
-button_1_text:`{button_1_text}`,
-button_2_text:`{button_2_text}`
+'correct_answer' : {correct_answer},
+'question':`{question}`,
+'right_answer_message':`{right_answer_message}`,
+'wrong_answer_message':`{wrong_answer_message}`,
+'button_1_text':`{button_1_text}`,
+'button_2_text':`{button_2_text}`
         }},"""
         f.write(main_string)
         # f.write(",".join([str(correct_answer),str(question),str(right_answer_message),str(wrong_answer_message),str(button_1_text),str(button_2_text)])+"\n")
